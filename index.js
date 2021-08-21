d95110b1a07814526061858b2b854adcff415a42
Debrabander <gautier.debrabander@gmail.com>
2021-08-21 13:22:02 +0200

    init

---
+++ index.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const http = require('http');
+const cors = require('cors');
+
+const PORT = 8080;
+
+const app = express();
+
+const index = require('./routes/index');
+
+app.use(express.static(`${__dirname}/public`));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(index);
+
+const server = http.createServer(app);
+server.listen(PORT, () => {
+  console.log(`api-abilitiz listening on port ${PORT}!`);
+});
